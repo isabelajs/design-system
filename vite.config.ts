0a71837cd382ab052b7da311b2277eea83ac7433
isabelajs <isabelajimenezs96@gmail.com>
2025-02-20 08:47:58 -0500

    enhance Vite configuration for library support with TypeScript and CSS injection

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname, relative, extname } from 'path'
+import { fileURLToPath } from 'url'
+import { glob } from 'glob'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ['lib'] })
+  ],
+  build:{
+    copyPublicDir: false,
+    lib:{
+      entry: resolve(__dirname, 'lib/main.js'),
+      name:'design-system',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob.sync('lib/**/*.{ts,tsx}', {
+          ignore: ["lib/**/*.d.ts"],
+        }).map(file => [
+          relative(
+            'lib',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      }
+    }
+  }
 })
