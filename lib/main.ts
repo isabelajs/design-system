14c87b174eaafe9b3bf9cced68569faa5d8c5064
isabelajs <isabelajimenezs96@gmail.com>
2025-02-20 16:06:19 -0500

    add main module to export Button, Text, Icon, and Input components

---
+++ lib/main.ts
@@ -0,0 +1,6 @@
+import Button from './components/Atomos/Button';
+import Text from './components/Atomos/Text';
+import Icon from './components/Atomos/Icons';
+import Input from './components/Atomos/Input';
+
+export { Button, Text,Icon,Input };
